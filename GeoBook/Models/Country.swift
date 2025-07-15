09a8f47c388722cb385508e832d44f6e78034b74
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-07-15 18:04:00 +0300

    Был создан файл Storyboard с 2 экранами (список стран и детальной информацией о выбранной стране).

---
+++ GeoBook/Models/Country.swift
@@ -0,0 +1,8 @@
+//
+//  Country.swift
+//  GeoBook
+//
+//  Created by Alexandr Artemov on 15.07.2025.
+//
+
+import Foundation
---
+++ GeoBook/ViewControllers/CountryDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CountryDetailViewController.swift
+//  GeoBook
+//
+//  Created by Alexandr Artemov on 15.07.2025.
+//
+
+import UIKit
+
+class CountryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
