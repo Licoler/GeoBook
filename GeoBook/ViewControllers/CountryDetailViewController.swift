2296975c219cd662cccda7251268d1d86da718d7
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-07-17 01:00:14 +0300

    Был создан метод для обновлении интерфейса при инициализации экрана.

--- GeoBook/ViewControllers/CountryDetailViewController.swift
+++ GeoBook/ViewControllers/CountryDetailViewController.swift
@@ -20,5 +20,15 @@ final class CountryDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureUI()
+    
+    private func configureUI() {
+        title = mock.name
+        flagImageView.image = UIImage(named: mock.image) ?? UIImage(named: "NoPhoto")
+        capitalLabel.text = "Столица: \(mock.capital)"
+        countryPhoneCodeLabel.text = "Код страны: \(mock.countryPhoneCode)"
+        countryAreaLabel.text = "Площадь страны: \(mock.countryArea) км²"
+        currencyLabel.text = "Местная валюта: \(mock.currency)"
+    }
     }
 }
