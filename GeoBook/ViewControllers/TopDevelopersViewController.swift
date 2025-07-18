ce4154884295791fc7e3b5ac840f817e62fe17aa
Neyilanau <halapovalbek@gmail.com>
2025-07-18 21:42:52 +0400

    Добавлен экран профиля и отображение команды

---
+++ GeoBook/ViewControllers/TopDevelopersViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TopDevelopersViewController.swift
+//  GeoBook
+//
+//  Created by Альбек Халапов on 18.07.2025.
+//
+
+import UIKit
+
+class TopDevelopersViewController: UIViewController {
+
+    @IBOutlet weak var profileImageViewFirst: UIImageView!
+    @IBOutlet weak var profileImageViewSecond: UIImageView!
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        [profileImageViewFirst, profileImageViewSecond].forEach { imageView in
+            imageView?.layer.cornerRadius = imageView!.frame.width / 2
+            imageView?.clipsToBounds = true
+        }
+    }
+}
