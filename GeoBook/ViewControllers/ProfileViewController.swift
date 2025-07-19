7ada079b74075ca3221ab53228b00fd3e437b79d
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-07-19 13:40:53 +0300

    Классы были сделаны финальными (final).

--- GeoBook/ViewControllers/ProfileViewController.swift
+++ GeoBook/ViewControllers/ProfileViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+final class ProfileViewController: UIViewController {
     
     @IBOutlet var userNameProfile: UITextField!
     @IBOutlet var passwordProfile: UITextField!
